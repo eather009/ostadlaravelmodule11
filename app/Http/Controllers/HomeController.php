d52d8663ca48ae5c4fb148d92fee062ef43a68b3
OSAKA-IFTEKHAR <eather@export-japan.co.jp>
2023-12-20 01:06:37 +0900

    update ui added dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,5 +16,27 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function dashboard(){
+        $saleSummaryToday = DB::table('sales')
+            ->whereDate('sale_date', Carbon::today())
+            ->sum('sale_amount');
+
+        $saleSummaryYesterday = DB::table('sales')
+            ->whereDate('sale_date', Carbon::yesterday())
+            ->sum('sale_amount');
+
+        $saleSummaryThisMonth = DB::table('sales')
+            ->whereMonth('sale_date', Carbon::now()->month)
+            ->whereYear('sale_date', Carbon::now()->year)
+            ->sum('sale_amount');
+
+        $saleSummaryLastMonth = DB::table('sales')
+            ->whereMonth('sale_date', Carbon::now()->subMonth()->month)
+            ->whereYear('sale_date', Carbon::now()->subMonth()->year)
+            ->sum('sale_amount');
+
+        return view('dashboard', compact('saleSummaryToday', 'saleSummaryYesterday', 'saleSummaryThisMonth', 'saleSummaryLastMonth'));
     }
 }
